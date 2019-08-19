78b24d923e25615a67b72bc7718336df62daa01c
Student <example@example.com>
2019-08-19 16:29:22 -0700

    completed

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var favFood = $("#favFood").val();
+alert(favFood);
+
+var favMusic = $("#favMusic").val();
+alert(favMusic);
+
+var favAirport = $("input:radio[name=favAirport]:checked").val();
+alert(favAirport);
